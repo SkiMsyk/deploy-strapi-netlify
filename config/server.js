f26cb01c1092292ee560fe5bb60af74a52ce1434
SkiMsyk <Sakai.Masayuki@keio.jp>
2021-02-07 01:19:34 +0000

    Initial Commite

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0079de607e87ab459035ee60c291a515'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dc87523a-7810-4f63-9672-694e55c7d7d9'
+};
